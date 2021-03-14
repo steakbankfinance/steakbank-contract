a14365a865d1dc0eae1267960f078047a58a6e81
pengqibc <pengqibc@gmail.com>
2021-03-15 00:46:34 +0800

    allow to accelerate multiple steps

--- contracts/StakeBNBAgent.sol
+++ contracts/StakeBNBAgent.sol
@@ -56,7 +56,6 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
     mapping(address => uint256) public stakingReward;
 
     uint256 public priceToAccelerateUnstake;
-    uint256 public nonAccelerateLength;
 
     uint256 public rewardPerStaking;
 
@@ -68,7 +67,7 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
     event Paused(address account);
     event Unpaused(address account);
     event ReceiveDeposit(address from, uint256 amount);
-    event AcceleratedUnstakedBNB(address AcceleratedStaker, address priorStaker, uint256 AcceleratedUnstakeIdx);
+    event AcceleratedUnstakedBNB(address AcceleratedStaker, uint256 AcceleratedUnstakeIdx);
     event Deposit(address from, uint256 amount);
 
     constructor() public {}
@@ -123,8 +122,7 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
         address _stakingRewardMaintainer,
         address _stakingRewardVault,
         address _unstakeVault,
-        uint256 _priceToAccelerateUnstake,
-        uint256 _nonAccelerateLength
+        uint256 _priceToAccelerateUnstake
     ) external initializer{
         admin = _admin;
 
@@ -138,7 +136,6 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
         unstakeVault = _unstakeVault;
 
         priceToAccelerateUnstake = _priceToAccelerateUnstake;
-        nonAccelerateLength = _nonAccelerateLength;
     }
 
     function paused() public view returns (bool) {
@@ -184,10 +181,6 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
 
     function setPriceToAccelerateUnstake(uint256 newPriceToAccelerateUnstake) onlyAdmin external {
         priceToAccelerateUnstake = newPriceToAccelerateUnstake;
-    }
-
-    function setNonAccelerateLength(uint256 newNonAccelerateLength) onlyAdmin external {
-        nonAccelerateLength = newNonAccelerateLength;
     }
 
     function stakeBNB(uint256 amount) nonReentrant mustInMode(NormalPeriod) whenNotPaused external payable returns (bool) {
@@ -244,38 +237,46 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
         return true;
     }
 
-    function accelerateUnstakedMature(uint256 unstakeIndex) nonReentrant whenNotPaused external returns (bool) {
-        require(unstakeIndex>headerIdx.add(nonAccelerateLength) && unstakeIndex<=tailIdx, "unstakeIndex is out of accelerate range");
+    function accelerateUnstakedMature(uint256 unstakeIndex, uint256 steps) nonReentrant whenNotPaused external returns (bool) {
+        require(steps > 0, "accelerate steps must be greater than zero");
+        require(unstakeIndex.sub(steps)>=headerIdx && unstakeIndex<=tailIdx, "unstakeIndex is out of valid accelerate range");
 
-        Unstake memory priorUnstake = unstakesMap[unstakeIndex-1];
         Unstake memory unstake = unstakesMap[unstakeIndex];
         require(unstake.staker==msg.sender, "only staker can accelerate itself");
 
-        uint256 skbBurnAmount = unstake.amount.mul(priceToAccelerateUnstake);
+        uint256 skbBurnAmount = unstake.amount.mul(steps).mul(priceToAccelerateUnstake);
+        for (uint256 idx = unstakeIndex-1 ; idx >= unstakeIndex.sub(steps); idx--) {
+            Unstake memory priorUnstake = unstakesMap[idx];
+            unstakesMap[idx+1] = priorUnstake;
+            skbBurnAmount = skbBurnAmount.add(priorUnstake.amount.mul(priceToAccelerateUnstake));
+            uint256[] storage priorUnstakeSeqs = accountUnstakeSeqsMap[priorUnstake.staker];
+            bool found = false;
+            for(uint256 i=0; i < priorUnstakeSeqs.length; i++) {
+                if (priorUnstakeSeqs[i]==idx) {
+                    priorUnstakeSeqs[i]=idx+1;
+                    found = true;
+                    break;
+                }
+            }
+            require(found, "failed to find matched unstake sequence");
+        }
+
+        uint256[] storage unstakeSeqs = accountUnstakeSeqsMap[msg.sender];
+        unstakesMap[unstakeIndex-steps] = unstake;
+        bool found = false;
+        for(uint256 idx=0; idx < unstakeSeqs.length; idx++) {
+            if (unstakeSeqs[idx]==unstakeIndex) {
+                unstakeSeqs[idx] = unstakeIndex - steps;
+                found = true;
+                break;
+            }
+        }
+        require(found, "failed to find matched unstake sequence");
+
         IERC20(SKB).safeTransferFrom(msg.sender, address(this), skbBurnAmount);
         IMintBurnToken(SKB).burn(skbBurnAmount);
 
-        uint256[] storage unstakeSeqs = accountUnstakeSeqsMap[msg.sender];
-        uint256[] storage priorUnstakeSeqs = accountUnstakeSeqsMap[priorUnstake.staker];
-
-        unstakesMap[unstakeIndex-1] = unstake;
-        unstakesMap[unstakeIndex] = priorUnstake;
-
-        for(uint256 idx=0; idx < unstakeSeqs.length; idx++) {
-            if (unstakeSeqs[idx]==unstakeIndex) {
-                unstakeSeqs[idx] = unstakeIndex - 1;
-                break;
-            }
-        }
-
-        for(uint256 idx=0; idx < priorUnstakeSeqs.length; idx++) {
-            if (priorUnstakeSeqs[idx]==unstakeIndex-1) {
-                priorUnstakeSeqs[idx] = unstakeIndex;
-                break;
-            }
-        }
-
-        emit AcceleratedUnstakedBNB(msg.sender, priorUnstake.staker, unstakeIndex);
+        emit AcceleratedUnstakedBNB(msg.sender, unstakeIndex);
 
         return true;
     }
@@ -324,13 +325,19 @@ contract StakingBNBAgent is Context, Initializable, ReentrancyGuard {
 
             uint256[] storage unstakeSeqs = accountUnstakeSeqsMap[unstake.staker];
             uint256 lastSeq = unstakeSeqs[unstakeSeqs.length-1];
-            unstakeSeqs.pop();
-            for(uint256 index=0; index < unstakeSeqs.length; index++) {
-                if (unstakeSeqs[index]==headerIdx) {
-                    unstakeSeqs[index] = lastSeq;
-                    break;
+            if (lastSeq != headerIdx) {
+                bool found = false;
+                for(uint256 index=0; index < unstakeSeqs.length; index++) {
+                    if (unstakeSeqs[index]==headerIdx) {
+                        unstakeSeqs[index] = lastSeq;
+                        found = true;
+                        break;
+                    }
                 }
+                require(found, "failed to find matched unstake sequence");
             }
+            unstakeSeqs.pop();
+
             headerIdx++;
         }
     }
